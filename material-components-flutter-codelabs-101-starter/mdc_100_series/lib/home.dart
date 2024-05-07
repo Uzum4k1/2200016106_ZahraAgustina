f4a629ed71496bb0a4c01a6f29ccef6c8e9f2ab0
Uzum4k1 <zahraagustina384@gmail.com>
2024-05-07 14:28:32 +0700

    prak6

--- material-components-flutter-codelabs-101-starter/mdc_100_series/lib/home.dart
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/home.dart
@@ -1,67 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
+import 'supplemental/asymmetric_view.dart';
 import 'model/product.dart';
 import 'model/products_repository.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    List<Card> _buildGridCards(BuildContext context) {
-      List<Product> products = ProductsRepository.loadProducts(Category.all);
+  // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(BuildContext context) {
+    List<Product> products = ProductsRepository.loadProducts(Category.all);
 
-      if (products.isEmpty) {
-        return const <Card>[];
-      }
-
-      final ThemeData theme = Theme.of(context);
-      final NumberFormat formatter = NumberFormat.simpleCurrency(
-        locale: Localizations.localeOf(context).toString(),
-      );
-
-      return products.map((product) {
-        return Card(
-          clipBehavior: Clip.antiAlias,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              AspectRatio(
-                aspectRatio: 18 / 11,
-                child: Image.asset(
-                  product.assetName,
-                  package: product.assetPackage,
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        product.name,
-                        style: theme.textTheme.titleLarge,
-                        maxLines: 1,
-                      ),
-                      const SizedBox(height: 8.0),
-                      Text(
-                        formatter.format(product.price),
-                        style: theme.textTheme.titleSmall,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      }).toList();
+    if (products.isEmpty) {
+      return const <Card>[];
     }
 
+    final ThemeData theme = Theme.of(context);
+    final NumberFormat formatter = NumberFormat.simpleCurrency(
+      locale: Localizations.localeOf(context).toString(),
+    );
+
+    return products.map((product) {
+      return Card(
+        clipBehavior: Clip.antiAlias,
+        // TODO: Adjust card heights (103)
+        elevation: 0.0,
+        child: Column(
+          // TODO: Center items on the card (103)
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18 / 11,
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                // TODO: Adjust the box size (102)
+                fit: BoxFit.fitWidth,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                child: Column(
+                  // TODO: Align labels to the bottom and center (103)
+                  mainAxisAlignment: MainAxisAlignment.end,
+  crossAxisAlignment: CrossAxisAlignment.center,
+                  // TODO: Change innermost Column (103)
+                  children: <Widget>[
+                     // TODO: Handle overflowing labels (103)
+                    Text(
+    product.name,
+    style: theme.textTheme.button,
+    softWrap: false,
+    overflow: TextOverflow.ellipsis,
+    maxLines: 1,
+  ),
+  const SizedBox(height: 4.0),
+  Text(
+    formatter.format(product.price),
+    style: theme.textTheme.caption,
+  ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+
+   // TODO: Add a variable for Category (104)
+  @override
+  Widget build(BuildContext context) {
+     // TODO: Return an AsymmetricView (104)
+    // TODO: Pass Category variable to
     return Scaffold(
+       // TODO: Add app bar (102)
       appBar: AppBar(
         title: const Text('SHRINE'),
         leading: IconButton(
@@ -69,7 +84,7 @@ class HomePage extends StatelessWidget {
             Icons.menu,
             semanticLabel: 'menu',
           ),
-          onPressed: () {
+          onPressed: (){
             print('Menu Button');
           },
         ),
@@ -94,12 +109,12 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        children: _buildGridCards(context),
-      ),
+
+       // TODO: Add a grid view (102)
+      body: AsymmetricView(
+  products: ProductsRepository.loadProducts(Category.all),
+),
+      // TODO: Set resizeToAvoidBottomInset (101)
     );
   }
 }
--- material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
+++ material-components-flutter-codelabs-101-starter/mdc_100_series/lib/login.dart
@@ -20,45 +20,63 @@ class _LoginPageState extends State<LoginPage> {
           children: <Widget>[
             const SizedBox(height: 80.0),
             Column(
-              children: <Widget>[
-                Image.asset('assets/diamond.png'),
-                const SizedBox(height: 16.0),
-                const Text('SHRINE'),
-              ],
-            ),
+  children: <Widget>[
+    Image.asset('assets/diamond.png'),
+    const SizedBox(height: 16.0),
+    Text(
+      'SHRINE',
+      style: Theme.of(context).textTheme.headlineSmall,
+    ),
+  ],
+),
             const SizedBox(height: 120.0),
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
+            // Remove filled: true values (103)
+TextField(
+  controller: _usernameController,
+  decoration: const InputDecoration(
+    // Removed filled: true
+    labelText: 'Username',
+  ),
+),
+const SizedBox(height: 12.0),
+TextField(
+  controller: _passwordController,
+  decoration: const InputDecoration(
+    // Removed filled: true
+    labelText: 'Password',
+  ),
+  obscureText: true,
+),
             const SizedBox(height: 24.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
                 TextButton(
-                  child: const Text('CANCEL'),
-                  onPressed: () {
-                    _usernameController.clear();
-                    _passwordController.clear();
-                  },
-                ),
+  child: const Text('CANCEL'),
+  onPressed: () {
+    _usernameController.clear();
+    _passwordController.clear();
+  },
+  style: TextButton.styleFrom(
+    foregroundColor: Theme.of(context).colorScheme.secondary,
+    shape: const BeveledRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+    ),
+  ),
+),
                 const SizedBox(width: 8.0),
                 ElevatedButton(
-                  child: const Text('NEXT'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
+  child: const Text('NEXT'),
+  onPressed: () {
+    Navigator.pop(context);
+  },
+  style: ElevatedButton.styleFrom(
+    elevation: 8.0,
+    shape: const BeveledRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(7.0)),
+    )
+  ),
+),
               ],
             ),
           ],
